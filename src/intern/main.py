507e0bc94fc1cb264b20d26e08b20bf91816d805
Akutagawa <pbwfh384@gmail.com>
2020-05-31 23:28:50 +0900

    fix path

--- src/intern/main.py
+++ src/intern/main.py
@@ -14,11 +14,11 @@ def main():
     device = torch.device(f"cuda:{gpu_id}" if gpu_id >= 0 else "cpu")
 
     # Load Dataset
-    DATA_ROOT = '/mnt/hinoki/yoshikoshi/sotsuron/dataset/pairpro'
+    DATA_ROOT = 'data/original'
 
-    train_path = osp.join(DATA_ROOT, 'train.txt')
-    dev_path = osp.join(DATA_ROOT, 'dev.txt')
-    test_path = osp.join(DATA_ROOT, 'test.txt')
+    train_path = osp.join(DATA_ROOT, 'Train_Data_F.pickle')
+    dev_path = osp.join(DATA_ROOT, 'Valid_Data_F.pickle')
+    test_path = osp.join(DATA_ROOT, 'Test_Data_F.pickle')
 
     train.run_bert(
             train_path,
