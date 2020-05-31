f067ca56442fac147552edca54f7a1a8064373d1
Akutagawa <pbwfh384@gmail.com>
2020-05-31 20:36:20 +0900

    get char2idx mapping

--- src/intern/make_embedding.py
+++ src/intern/make_embedding.py
@@ -1,9 +1,26 @@
 from collections import Counter
+import pickle
 
 
 def count_char(sentences):
     sentences = ''.join(sentences)
     co = Counter(sentences)
-    co = [v for v in co if v[1] >= 2]
+
     sorted_co = sorted(co.items(), key=lambda x: x[1], reverse=True)
-    return sorted_co
+    print(sorted_co)
+    sorted_co = [v for v in sorted_co if v[1] >= 2]
+
+    char2idx = {}
+    for i, v in enumerate(sorted_co):
+        char2idx[v[0]] = i
+
+    return char2idx
+
+
+if __name__ == '__main__':
+    DATA = 'data/original/Train_Data_F.pickle'
+
+    with open(DATA, 'rb') as f:
+        sentences = pickle.load(f)
+    char2idx = count_char(sentences)
+    print(char2idx)
