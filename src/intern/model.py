390e9200f1f13b5f331512e902db42ae80e7d0b3
Akutagawa <pbwfh384@gmail.com>
2020-06-01 13:18:24 +0900

    change to bi-lstm

--- src/intern/model.py
+++ src/intern/model.py
@@ -1,17 +1,36 @@
 import torch
 import torch.nn as nn
+import torch.functional as F
 
 
 class LSTM_divider(nn.Module):
-    def __init__(self, voc_size, w_dims=128, h_dims=100, dropout=0.1):
+    def __init__(
+            self,
+            voc_size,
+            w_dims=128,
+            h_dims=100,
+            num_layers=1,
+            bidirectional=True,
+            dropout=0.1,
+        ):
         super(LSTM_divider, self).__init__()
         self.voc_size = voc_size
         self.w_dims = w_dims
         self.h_dims = h_dims
         self.embdic = nn.Embedding(self.voc_size, self.w_dims)
+        num_directions = 2 if bidirectional else 1
+        self.num_directions = num_directions
+        self.num_layers = num_layers
 
         # Define LSTM
-        self.lstm = nn.LSTM(self.w_dims, self.h_dims)
+        self.lstm = nn.LSTM(
+                self.w_dims,
+                self.h_dims,
+                num_layers=num_layers,
+                bidirectional=bidirectional)
+
+        self.relu = nn.ReLU()
+        self.l0 = nn.Linear(self.h_dims * 2, 1)
 
         # Define Dropout
         p = 0.1
@@ -34,19 +53,21 @@ class LSTM_divider(nn.Module):
         # (sent_len, batchsize, hdims) -> (batchsize, sent_len, hdims)
         unpacked_hidden = torch.transpose(unpacked_hidden, 0, 1)
 
-        # average on hidden dims
-        # (batchsize, sent_len, hdims) -> (batchsize, sent_len)
-        hidden_avg = torch.sum(unpacked_hidden, axis=-1) / self.h_dims
+        # (batchsize, sent_len, hdims) -> (batchsize, sent_len, 1)
+        out = self.l0(self.relu(unpacked_hidden))
 
-        out = torch.sigmoid(hidden_avg)
+        # (batchsize, sent_len) -> (batchsize, sent_len)
+        out = out.squeeze(-1)
+
+        out = torch.sigmoid(out)
 
         return out
 
     def init_lstm_state(self, batchsize, hdims, device=None):
         h, c = torch.zeros(batchsize, hdims), torch.zeros(batchsize, hdims)
         # (1, batchsize, hdims)
-        h = h.expand(1, batchsize, hdims)
-        c = c.expand(1, batchsize, hdims)
+        h = h.expand(self.num_layers * self.num_directions, batchsize, hdims)
+        c = c.expand(self.num_layers * self.num_directions, batchsize, hdims)
         if device is not None:
             h = h.to(device)
             c = c.to(device)
